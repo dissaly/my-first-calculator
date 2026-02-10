b2a053fe24cb0651eb80c8269e8239081826d7d3
Dima <playdmitry208@gmail.com>
2026-02-10 20:45:25 +0300

    Додав юніт тести

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
+import unittest
 print(os.path.abspath(__file__))
 def calculator():
-
     print("Калькулятор активовано")
     num1_vvid = (input("Введіть перше число: ").strip().replace("," , "."))
     alphavit = "йцукенгшщзхїфівапролджєґячсмитьюqwertyuiopasdfghjklzxcvbnm"
@@ -39,6 +39,25 @@ def calculator():
     print(output)
     fille = open("results.txt", "a" , encoding="utf-8")
     fille.write(output + "\n")
-    fille.close
+    fille.close()
 
 calculator() 
+def sum(a, b): 
+    return a + b
+def minus(a, b): 
+    return a - b
+def mul(a, b): 
+    return a * b
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Помилка: ділення на нуль"
+class testcalculator(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(sum(10, 20), 30)
+        self.assertEqual(sum(-1, 1), 0)
+    def test_minus(self):
+        self.assertEqual(minus(20, 10), 10)
+        self.assertEqual(minus(2, 1), 1)
+unittest.TestProgram()  
